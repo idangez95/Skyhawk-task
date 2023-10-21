92eed1897ac9c67d1b30ca251f885299dc2e0c21
idangez95 <idangez95@gmail.com>
2023-10-21 15:25:29 +0300

    adding logic to disable save buttons when there are errors, when inputs are empty. need to work on performance and separate code files

--- src/pages/users/UsersPage.jsx
+++ src/pages/users/UsersPage.jsx
@@ -1,17 +1,35 @@
 import UsersList from './usersList/UsersList';
 import PrimaryButton from '../../components/PrimaryButton';
 import styles from './users.module.css';
+import { useMemo, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { usersDataState } from '../../state/atoms/userDataState';
 
 function UsersPage() {
+  const [isValidRow, setIsValidRow] = useState(false);
+  const [usersData, setUsersData] = useRecoilState(usersDataState);
+  const [tempUsersData, setTempUsersData] = useState(usersData);
+
+  const hasEmptyValues = useMemo(
+    () => tempUsersData.some((user) => Object.values(user).some((value) => value === '')),
+    [tempUsersData]
+  );
+
+  const saveUsers = () => {
+    setUsersData(tempUsersData);
+  };
+
   return (
     <div className={styles.pageRoot}>
       <div className={styles.pageContentContainer}>
-        <UsersList />
+        <UsersList
+          setIsValidRow={setIsValidRow}
+          usersData={usersData}
+          tempUsersData={tempUsersData}
+          setTempUsersData={setTempUsersData}
+        />
         <div className={styles.rightButtonContainer}>
-          <PrimaryButton
-            disabled={false}
-            // TODO: Implement onClick handler
-          >
+          <PrimaryButton disabled={!isValidRow || hasEmptyValues} handleClick={saveUsers}>
             Save
           </PrimaryButton>
         </div>
